f9e56ddd4befca9a2c6f84232789a7ebf3c3216e
Stian Berg Johansen <stian@riktigspor.no>
2021-02-02 10:09:11 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
 
         <div class="main-footer__colophon  padding-top-xs margin-top-lg">
             <div class="text-sm text-xs@md color-contrast-medium flex flex-wrap gap-xs">
-                Test<?php the_field( 'sub_footer_content', 'option' ); ?>
+                <?php the_field( 'sub_footer_content', 'option' ); ?>
             </div>
         </div>
     </div>
