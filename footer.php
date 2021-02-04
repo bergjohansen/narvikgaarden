144c6da828292c0a9b863378c3178fd592235d3f
Stian Berg Johansen <stian@riktigspor.no>
2021-02-04 10:09:54 +0100

    footer
    Lagt til SoMe

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@
                 <ul class="grid gap-lg">
                     <li class="col-6@xs col-4@md padding-y-md@sm">
                         <h4 class="margin-bottom-sm text-base@md font-normal"><?php the_field( 'footer_contact', 'option' ); ?></h4>
-                        <?php the_field( 'footer_contact_content', 'option' ); ?>
+                        <?php the_field( 'footer_contact_content', 'option' ); ?><br>
+
                     </li>
 
                     <li class="col-6@xs col-4@md padding-y-md@sm">
@@ -26,9 +27,24 @@
                 </ul>
             </nav>
         </div>
+        <div class="main-footer__colophon  padding-top-xs">
 
-        <div class="main-footer__colophon  padding-top-xs margin-top-lg">
-            <div class="text-sm text-xs@md color-contrast-medium flex flex-wrap gap-xs">
+              <p>
+              <a href="#" class="margin-bottom-sm padding-right-md font-light"><svg class="icon margin-right-xs" viewBox="0 0 32 32"><title>Følg oss på Facebook</title><path d="M32,16A16,16,0,1,0,13.5,31.806V20.625H9.438V16H13.5V12.475c0-4.01,2.389-6.225,6.043-6.225a24.644,24.644,0,0,1,3.582.312V10.5H21.107A2.312,2.312,0,0,0,18.5,13v3h4.438l-.71,4.625H18.5V31.806A16,16,0,0,0,32,16Z"></path></svg>Narvikgården</a>
+              <a href="https://www.facebook.com/pages/Parkhallen-Parkering/1577221435869307" class="margin-bottom-sm padding-right-md font-light"><svg class="icon margin-right-xs " viewBox="0 0 32 32"><title>Følg oss på Facebook</title><path d="M32,16A16,16,0,1,0,13.5,31.806V20.625H9.438V16H13.5V12.475c0-4.01,2.389-6.225,6.043-6.225a24.644,24.644,0,0,1,3.582.312V10.5H21.107A2.312,2.312,0,0,0,18.5,13v3h4.438l-.71,4.625H18.5V31.806A16,16,0,0,0,32,16Z"></path></svg>Parkhallen Parkering</a>
+              <a href="https://www.facebook.com/narvikbobilparkering/" class="margin-bottom-sm padding-right-md font-light"><svg class="icon margin-right-xs align-middle" viewBox="0 0 32 32"><title>Følg oss på Facebook</title><path d="M32,16A16,16,0,1,0,13.5,31.806V20.625H9.438V16H13.5V12.475c0-4.01,2.389-6.225,6.043-6.225a24.644,24.644,0,0,1,3.582.312V10.5H21.107A2.312,2.312,0,0,0,18.5,13v3h4.438l-.71,4.625H18.5V31.806A16,16,0,0,0,32,16Z"></path></svg>Narvik Bobilparkering</a>
+              <a href="https://www.facebook.com/fagernesfjelletparkering/" class="margin-bottom-sm padding-right-md font-light"><svg class="icon margin-right-xs" viewBox="0 0 32 32"><title>Følg oss på Facebook</title><path d="M32,16A16,16,0,1,0,13.5,31.806V20.625H9.438V16H13.5V12.475c0-4.01,2.389-6.225,6.043-6.225a24.644,24.644,0,0,1,3.582.312V10.5H21.107A2.312,2.312,0,0,0,18.5,13v3h4.438l-.71,4.625H18.5V31.806A16,16,0,0,0,32,16Z"></path></svg>Fagernesfjellet Parkering</a>
+              <a href="https://www.instagram.com/parkeringinarvik/?hl=nb" class="margin-bottom-sm padding-right-md font-light color-success"><svg class="icon margin-right-xs" aria-hidden="true" width="16" height="16" viewBox="0 0 16 16"><g><circle cx="12.145" cy="3.892" r="0.96"></circle><path data-color="color-2" d="M8,12c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S10.206,12,8,12z M8,6C6.897,6,6,6.897,6,8 s0.897,2,2,2s2-0.897,2-2S9.103,6,8,6z"></path><path d="M12,16H4c-2.056,0-4-1.944-4-4V4c0-2.056,1.944-4,4-4h8c2.056,0,4,1.944,4,4v8C16,14.056,14.056,16,12,16z M4,2C3.065,2,2,3.065,2,4v8c0,0.953,1.047,2,2,2h8c0.935,0,2-1.065,2-2V4c0-0.935-1.065-2-2-2H4z"></path></g></svg>Parkering i Narvik</a><br><br>
+
+
+              </p>
+
+        </div>
+
+        <div class="main-footer__colophon  padding-top-xs margin-top-sm">
+
+
+              <div class="text-sm text-xs@md color-contrast-medium flex flex-wrap gap-xs">
                 <p class="text-xs text-xs@md"><span><?php echo get_bloginfo(); ?> © <?php echo date('Y'); ?></span>&nbsp; · &nbsp;<span><a href="/personvernvilkar/">Personvernerklæring</a></span>&nbsp; · &nbsp;<span>Design &amp; utvikling: <a href="https://riktigspor.no/" target="_blank">Riktig Spor</a></span></p>
                  <?php the_field( 'sub_footer_content', 'option' ); ?>
             </div>
